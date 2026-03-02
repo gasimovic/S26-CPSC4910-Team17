f9fb8aeb77f1203b6e50f872dcbf850dda8fc849
ColeDiGregorio4 <cdigreg@g.clemson.edu>
2026-03-02 18:00:33 -0500

    Removing test

--- backend/test2.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const credentials = Buffer.from('ColeDiGr-Goo-SBX-f4eb38236-93def868:SBX-4eb382362a0b-4dd7-4518-a1a9-323c').toString('base64');
-axios.post('https://api.sandbox.ebay.com/identity/v1/oauth2/token', 
-  new URLSearchParams({
-    grant_type: 'client_credentials',
-    scope: 'https://api.ebay.com/oauth/api_scope'
-  }).toString(), 
-  { headers: { 'Content-Type': 'application/x-www-form-urlencoded', Authorization: `Basic ${credentials}` } }
-).then(res => console.log(res.data)).catch(err => console.error(err.response?.data || err.message));
--- test_ebay.js
+++
@@ -1,7 +0,0 @@
-const { getEbayToken } = require('./backend/utils/ebayTokenManager.js');
-
-getEbayToken().then(token => {
-    console.log("Token success:", token.substring(0, 10) + '...');
-}).catch(err => {
-    console.error("Token error:", err.message);
-});
